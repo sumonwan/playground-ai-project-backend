25f63078a15df5c72fe54e6f20f92b74dbf84568
Sumonwan Sammakul <sumonwan.s@kbtg.tech>
2025-09-18 13:48:16 +0700

    ✨ Init project

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func main() {
+	// Create fiber app
+	app := fiber.New()
+
+	// Middleware
+	app.Use(logger.New())
+	app.Use(cors.New())
+
+	// Routes
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"message": "hello world",
+		})
+	})
+
+	// Start server on port 3000
+	log.Fatal(app.Listen(":3000"))
+}
